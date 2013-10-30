8f041e173b7ca0bf8cfc6d0e6d2aa3ac0b962deb
CNUIEEE <jackson.sardello.11@cnu.edu>
2013-10-29 21:59:36 -0400

    Back to master
    meow

--- FDAGUI.java
+++ FDAGUI.java
@@ -2,10 +2,8 @@
 public class FDAGUI {
    public static void main(String[] args) {
        //I'm testing branches
-<<<<<<< HEAD
        //This is the comment made by Jackson's Branch
-=======
        //This is the commit made by the dup branch
->>>>>>> origin/Dup-Branch
+
    }
 }
