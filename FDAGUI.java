c9b9a65c35c63b23307b6b340715197d1f50d5c0
CNUIEEE <jackson.sardello.11@cnu.edu>
2013-10-30 08:02:15 -0400

    Still testing branching

--- FDAGUI.java
+++ FDAGUI.java
@@ -2,8 +2,6 @@
 public class FDAGUI {
    public static void main(String[] args) {
        //I'm testing branches
-       //This is the comment made by Jackson's Branch
-       //This is the commit made by the dup branch
 
    }
 }
