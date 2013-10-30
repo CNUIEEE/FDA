1ea0eea01cd8e71d978342246d941ae3e73e2c07
CNUIEEE <jackson.sardello.11@cnu.edu>
2013-10-30 08:49:24 -0400

    okay

--- FDAGUI.java
+++ FDAGUI.java
@@ -1,10 +1,5 @@
-
 public class FDAGUI {
-   public static void main(String[] args) {
-<<<<<<< HEAD
-   //Kayla's Comment
-=======
-   //Jackson's comment
->>>>>>> origin/master
-   }
+    public static void main(String[] args) {
+
+    }
 }
